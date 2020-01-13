f5bf6affcec07717cfcd2eac97aba9af39c14bc0
charan220 <raavicharan33@gmail.com>
2020-01-13 12:51:15 +0530

    [Charan]Added.node_modules in gitignore

--- Examples/arrayexplanation.js
+++
@@ -1,4 +0,0 @@
-let list=[1,2,3,4,5,5,7,5,8]
-let b=[5,6];
-b.push(...list);
-console.log(Math.max(...b));
--- Examples/jsonparse.js
+++
@@ -1,7 +0,0 @@
-let text='{ "name":"John", "age":30, "city":"New York"}'
-var obj = JSON.parse(text);
-console.log(`name is ${obj.name} and city is ${obj.city} ` )
-
-$.getJSON("test.js",function(data){
-    console.log("JSON Dtaa:",data.users[1].name)
-})
